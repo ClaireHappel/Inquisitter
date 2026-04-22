8677fe5da486694b65d2129af7cf58eebcaecfda
Claire Happel <clairehap1@gmail.com>
2026-04-22 12:35:54 -0500

    added username, text, timestamp, user friendly timestamp, and button

--- index.js
+++ index.js
@@ -7,19 +7,52 @@ $(() => {
   const $tweetsDiv = $('<div class="tweets"></div>');
   $page.append($tweetsDiv);
 
-  // an array of divs, logic for tweet creation, FIRST PUT THIS ALL IN A FUNC
-  const $tweets = streams.home.map((tweet) => {
-    // THIS WILL NAME TO CHANGE, USERNAME NEEDS TO BE IN OWN TAG TO BE CLICKABLE
-    const $tweet = $('<div class="tweet"></div>');
-    // NEEDS TO BE WITHIN ITS OWN TAG WITHIN THE DIV
-    const text = `@${tweet.user}: ${tweet.message}`;
-    // END CHANGES
-    // GIVE THE USERNAME IT'S CLICK HANDLER FUNCTION INSIDE HERE
-    $tweet.text(text);
-    return $tweet;
-  });
-  $tweetsDiv.append($tweets);
-  // CODE TO PUT IN FUNCTION ENDS HERE
+  // CREATE BUTTON
+
+  // 1. Create the button element with the required ID
+  const $button = $('<button id="new-tweets-button">Show New Tweets</button>');
+  // 2. Add the button to the page (e.g., at the top of the body or before the .tweets div)
+  $('.tweets').before($button);
+
+  // NEW TWEETS FUNCTION
+  const newTweet = function () {
+  // an array of divs, logic for tweet creation
+    const $tweets = streams.home.slice().reverse().map((tweet) => {
+      // our tweet, sad and empty- that needs data
+      const $tweet = $('<div class="tweet"></div>');
+
+      // create a username, using a div creates a block look, using span puts everything on one line
+      const $username = $('<div class="user"></div>');
+      $username.text(`@${tweet.user}: `);
+
+      // create a message
+      const $message = $('<div class="message"></div>');
+      $message.text(tweet.message);
+
+      // create a timestamp
+      const $timestamp = $('<div class="timestamp"></div>');
+      $timestamp.text(moment(tweet.created_at).format('MMMM Do YYYY, h:mm:ss a'));
+
+      // create a friendly timestamp
+      const $friendly = $('<div class="humanFriendlyTimestamp"></div>');
+      $friendly.text(` (${moment(tweet.created_at).fromNow()})`);
+      // GIVE THE USERNAME IT'S CLICK HANDLER FUNCTION INSIDE HERE
+
+      // return our big fat tweet containing all of our delightful data
+      return $tweet.append($username, $message, $timestamp, $friendly);
+    });
+    //$tweetsDiv.append($tweets);
+    return $('.tweets').html($tweets);
+  };
+  // calling newTweet function
+  newTweet();
+
+  // ACTION TO ADD BUTTON FUNCTIONALITY
+  $button.on('click', newTweet);
+
+
+
+
 
   // TIPS:
   // YOU WILL NEED TO PUT NEW TWEETS ON TO THE PAGE EITHER WITH A BUTTON
@@ -29,7 +62,7 @@ $(() => {
   // IF USING BUTTON, NEW-TWEETS-BUTTON MUST BE ID FOR TEST
 
   // MOMENT FORMAT BELOW
-  moment().format('MMM Do YYYY, h:mm:ss a');
+  moment().format('MMMM Do YYYY, h:mm:ss a');
 
 
 
