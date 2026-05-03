4855de2e39d29a649b6cd088aa1cc6b2dc9ab9eb
ClaireHappel <clairehap1@gmail.com>
2026-05-02 19:32:30 -0500

    fixing gap

--- index.js
+++ index.js
@@ -1005,8 +1005,8 @@ $(document).ready(function() {
     'z-index': '10000',
     'position': 'fixed', // Added to match the inputs
     'left': 'clamp(5%, 16%, 40%)',
-    'transform': 'translate(103%, -40vh)',
-    'width': 'clamp(180px, 37vw, 655px)', // Raised min from 40px to 120px to fit text
+    'transform': 'translate(99%, -40vh)',
+    'width': 'clamp(200px, 38vw, 668px)', // Raised min from 40px to 120px to fit text
     'max-width': '80vw',
     'text-align': 'center',
     'white-space': 'nowrap',
